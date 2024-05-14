9b30d2f867d34c1bb750b087f8391efaffd3b9e6
surapaneni <zenith.surapaneni@phenompeople.com>
2024-05-14 14:15:56 +0530

    Bug fixes while uploading the images

--- frontend/src/components/Dashboards/ViewBug/ImageUploader.js
+++ frontend/src/components/Dashboards/ViewBug/ImageUploader.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 function ImageUploader({ onUpload, onUploadStart }) {
     const [imageFiles, setImageFiles] = useState([]);
+    const [progress, setProgress] = useState(0);
+    const [isUploading, setIsUploading] = useState(false);
 
     const uploadImage = async (file) => {
         const formData = new FormData();
@@ -10,7 +12,10 @@ function ImageUploader({ onUpload, onUploadStart }) {
 
         const response = await fetch(`https://api.cloudinary.com/v1_1/dvuycranr/image/upload`, {
             method: 'POST',
-            body: formData
+            body: formData,
+            onUploadProgress: (event) => {
+                setProgress(Math.round((100 * event.loaded) / event.total));
+            }
         });
 
         const data = await response.json();
@@ -20,14 +25,21 @@ function ImageUploader({ onUpload, onUploadStart }) {
     const handleFileChange = async (event) => {
         const files = event.target.files;
         onUploadStart(); // Notify that the upload process has started
+        setIsUploading(true); // Set isUploading to true when upload starts
+        setProgress(0);  // Reset progress to 0 when new upload starts
         const urls = await Promise.all(Array.from(files).map(file => uploadImage(file)));
         setImageFiles(urls);
         onUpload(urls);
+        setIsUploading(false); // Set isUploading to false when upload ends
+
+        // Clear file input
+        event.target.value = null;
     };
 
     return (
         <div>
             <input type="file" multiple onChange={handleFileChange} />
+            {isUploading && <progress value={progress} max="100" />}
             {imageFiles.map(url => (
                 <img key={url} src={url} alt="Uploaded" style={{ width: 100, height: 100 }} />
             ))}
